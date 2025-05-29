a9e44e3d530309c9ce1bd610852f54ff15183e63
Prasanth <50902596+prasanth100298@users.noreply.github.com>
2025-05-29 22:00:06 +0530

    added simple eventHandlers and state management

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,25 @@
+import { useState } from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 
 const ExpenseItem = (props) => {
 
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle('Updated!!');
+  };
+
   return (
     <div className='Expense-item'>
       <ExpenseDate date= {props.date}/>
       <div className='Expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='Expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </div>
   );
 }
