54b4399246fec69d3e779fce03a06ebef02736c3
Prasanth <50902596+prasanth100298@users.noreply.github.com>
2025-05-29 11:28:39 +0530

    added nested component Expense

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expense from "./components/Expenses";
 
 function App() {
-  const expense = [
+  const expenses = [
     { title: "Car Insurance", amount: 294.56, date: new Date(2025, 2, 28) },
     { title: "Toilet paper", amount: 94.56, date: new Date(2024, 7, 14) },
     { title: "New desk", amount: 450.0, date: new Date(2025, 5, 28) },
-  ];
+    ];
 
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expense[1].title}
-        amount={expense[1].amount}
-        date={expense[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expense[2].title}
-        amount={expense[2].amount}
-        date={expense[2].date}
-      ></ExpenseItem>
+      <Expense item={expenses}/>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
+
 
 function ExpenseItem(props) {
 
   return (
     <div className='Expense-item'>
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date= {props.date}/>
       <div className='Expense-item__description'>
         <h2>{props.title}</h2>
         <div className='Expense-item__price'>${props.amount}</div>
